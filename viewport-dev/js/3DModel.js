ea39ce21a879c6b3e61fc72da46b60a4329f945e
VikVelev <viktorvelev8@gmail.com>
2018-01-25 11:02:52 +0200

    OOP adjustments

--- viewport-dev/js/3DModel.js
+++ viewport-dev/js/3DModel.js
@@ -1,28 +1,42 @@
 class Model3D {
     //model = 3D THREE.js Geometry Object
-    constructor(model, scene, diffmode){
+    //constructor TOTHINK does name matter?
+    constructor( model, scene, diffmode ){
         //diffmode indicates the following - { active: false, type: "before"}
         this.diffmode = diffmode;
+        this.selected = false;
         this.scene = scene;
         this.model = model;
-        this.vertexNormals  = new THREE.VertexNormalsHelper(cube,0.15);
-        this.faceNormals = new THREE.FaceNormalsHelper(cube,0.15);
+        this.vertexNormals  = new THREE.VertexNormalsHelper( cube, 0.15 );
+        this.faceNormals = new THREE.FaceNormalsHelper( cube, 0.15 );
 
         this.wireframe = new THREE.LineSegments(
-                            new THREE.WireframeGeometry(this.model.geometry),
+                            new THREE.WireframeGeometry( this.model.geometry ),
                             new THREE.LineBasicMaterial({
                                 color: 0xffffff,
-                                linewidth: 2 
+                                linewidth: 2
                             }));
         //every model starts at the center, you can move it using the moveModel() method.
         this.model.position.x = 0;
         this.model.position.y = 0;
         this.model.position.z = 0;
 
+        THREE.SceneUtils.detach( this.faceNormals, this.model, this.scene );
+        THREE.SceneUtils.attach( this.faceNormals, this.scene, this.model );
+
+        THREE.SceneUtils.detach( this.vertexNormals, this.model, this.scene );
+        THREE.SceneUtils.attach( this.vertexNormals, this.scene, this.model );
+
         this.scene.add(this.model);
         this.scene.add(this.wireframe);
         this.scene.add(this.vertexNormals);
         this.scene.add(this.faceNormals);
+    
+        //These names are there so I can address all of these objects alter.
+        this.model.name = "mesh"
+        this.vertexNormals.name = "vnormals"
+        this.wireframe.name = "wireframe"
+        this.faceNormals.name = "fnormals"
 
         this.faceNormals.visible = false;
         this.vertexNormals.visible = false;
@@ -30,30 +44,30 @@ class Model3D {
     }
 
     toggleDiffMode(){
-        if (this.diffmode.active){
-            if(this.diffmode.type == "before"){
+        if ( this.diffmode.active ){
+            if( this.diffmode.type == "before" ){
 
                 this.wireframe.material = new THREE.LineBasicMaterial(
                 {
                     color: 0xff1212,
-                    linewidth: 2
+                    linewidth: 4.5
                 });
                 this.wireframe.visible = true;
 
 
-            } else if (this.diffmode.type == "after"){
+            } else if ( this.diffmode.type == "after" ){
 
                 this.wireframe.material = new THREE.LineBasicMaterial(
                 {
                     color: 0x12ff12,
-                    linewidth: 2
+                    linewidth: 4.5
                 });
                 this.wireframe.visible = true;
             }
         }
     }
 
-    moveModel(x,y,z){
+    moveModel( x, y, z ){
         this.model.position.x = x
         this.vertexNormals.position.x = x
         this.faceNormals.position.x = x
@@ -69,14 +83,13 @@ class Model3D {
         this.faceNormals.position.z = z
         this.wireframe.position.z = z
     }
-
+    //there are global toggle methods and local, these are the local ones.
     toggleVertexNormals(){
         this.vertexNormals.visible = !this.vertexNormals.visible
     }
 
     toggleFaceNormals(){
         this.faceNormals.visible = !this.faceNormals.visible
-        
     }
 
     toggleWireframe(){
--- viewport-dev/js/viewport.js
+++ viewport-dev/js/viewport.js
@@ -10,54 +10,118 @@ let faceNormals, vertexNormals, wireframe;
 init();
 animate();
 
-//TODO: Make the toggability of wireframe and normals global.
-
+//TODO: Implement selection.
+//TODO: OOP -> Viewport class, so I can run multiple instances of viewports
 function createCube(){
-	geometry = new THREE.CubeGeometry(0.4, 0.4, 0.4)
-	cube = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
+	geometry = new THREE.CubeGeometry( 0.4, 0.4, 0.4 )
+	cube = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
 	mesh = new Model3D(cube, scene, {active: true, type: "before"})
+
+	geometry = new THREE.CubeGeometry( 0.45, 0.45, 0.45 )
+	cube = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+	mesh2 = new Model3D( cube, scene, {active: true, type: "after"} )
+	mesh2.moveModel( 1, 0, 0 )
+
+	mesh.toggleDiffMode()
+	mesh2.toggleDiffMode()
+
+
 	//TODO: implement opacity if it's in diff mode
+}
+
+function createHelpers(){
+	gridHelper = new THREE.GridHelper( 10, 10, 0xffffff, 0x808080 );
+	gridHelper.position.x = 0;
+	gridHelper.position.y = 0;
+	gridHelper.position.z = 0;
+
+	var axes =  new THREE.AxesHelper( 10 )
+	scene.add( axes );
+	scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+	scene.add( gridHelper );
 }
 
 function init() {
 	//Initialising camera
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
-	camera.position.z = 1;
-	camera.position.y = 0.5;
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100/*set this to the mesh's size*/ );
 	camera.position.x = 0.5;
+	camera.position.y = 0.5;
+	camera.position.z = 1;
 
 	//Initialising misc.
-	controls = new THREE.OrbitControls(camera);
+	controls = new THREE.OrbitControls( camera );
+	controls.minDistance = 0; 
+	controls.maxDistance = 20; //set this to the mesh's size
+	controls.maxPolarAngle = Math.PI;
+
 	scene = new THREE.Scene();
 	loader = new THREE.OBJLoader();
-	
-	gridHelper = new THREE.GridHelper(10, 10, 0x00ff00, 0x808080);
-	gridHelper.position.x = 0;
-	gridHelper.position.y = 0;
-	gridHelper.position.z = 0;	
-	scene.add(gridHelper);
 
+	createHelpers()
 	//Initialising objects
 	createCube()
-	
+
 	//Initialising canvas/renderer
-	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer = new THREE.WebGLRenderer( {antialias: true} );
 
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-	window.addEventListener('resize', onWindowResize, false);
+	window.addEventListener( 'resize', onWindowResize, false );
 
 }
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(scene,camera);
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
 
+}
+
+function toggleVertexNormals(){
+	
+	scene.traverse ( function( object )
+	{
+		if ( object.name == 'vnormals' ){
+			object.visible = !object.visible
+		}
+	});
+
+}
+
+function toggleFaceNormals(){
+	scene.traverse (function (object)
+	{
+		if (object.name == 'fnormals'){
+			object.visible = !object.visible
+		}
+	});
+}
+
+function toggleWireframe(){
+	scene.traverse (function (object)
+	{
+		if (object.name == 'wireframe'){
+			object.visible = !object.visible
+		}
+	});	
+}
+
+function toggleTextures(){
+	//TODO: Implement this
+}
+
+function toggleMesh(){
+	scene.traverse (function (object)
+	{
+		if (object.name == 'mesh'){
+			object.visible = !object.visible
+		}
+	});
 }
