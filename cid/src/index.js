76fa652092f1e4db1fd97d554ea75d0cd8972d78
VikVelev <viktorvelev8@gmail.com>
2018-02-16 00:19:26 +0200

    Fixed index.js rewrite error

--- cid/src/index.js
+++ cid/src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import MainNavbar from './components/navbar.js';
+import Content from './components/content.js';
+import MainFooter from './components/footer.js';
+
+import './index.css';
+
+class Main extends Component {
+    render(){
+        return(
+            <div className="main">
+                <MainNavbar/>
+                <Content/>
+                <MainFooter/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render( <Main/>, document.getElementById("app"))
