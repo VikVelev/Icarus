fc19f98f3c894c82fd0766225c89968322bdc3b6
VikVelev <viktorvelev8@gmail.com>
2018-02-26 02:45:39 +0200

    Nonsense

--- cid-spa/src/components/navbar.js
+++ cid-spa/src/components/navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class Navbar extends Component {
     render(){
         return(
-            <div>????</div>
-        )
+          "lol"  
+        );
     }
 }
