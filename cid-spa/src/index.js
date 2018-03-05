20c34033c4019e6afdf1de296cfb2e8a9e55ccac
VikVelev <viktorvelev8@gmail.com>
2018-03-05 12:42:05 +0200

    Basic Redux set up

--- cid-spa/src/index.js
+++ cid-spa/src/index.js
@@ -2,16 +2,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import Content from './components/content.js';
-import cidReducers from './reducers/main.js'
+import store from './store'
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 
-let store = createStore(cidReducers)
 
 class Main extends Component {
     render(){
---
+++ cid-spa/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+
+import cidReducers from './reducers/main.js'
+
+const logger = createLogger()
+const middleware = applyMiddleware(promise(), thunk, logger)
+
+export default createStore(cidReducers, middleware)
