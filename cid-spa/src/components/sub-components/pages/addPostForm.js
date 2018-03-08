53bf5db99719807fa1e24d37cc4c5fcbd498f4cc
VikVelev <viktorvelev8@gmail.com>
2018-03-08 14:31:33 +0200

    Added Add Post design, Some minio tweaks, Almost working settings

---
+++ cid-spa/src/components/sub-components/pages/addPostForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Segment, Image, Header, Icon, Form, /*Message,*/ Button, Select } from 'semantic-ui-react'
+
+export default class AddPost extends Component{
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return (
+            <Segment className="addPostContainer profileSettingsForm">
+            <Header size="huge">Add post</Header>
+                <Form size='large' onSubmit={this.handleSubmit}>
+                    <Segment stacked>
+                        <Header>Title:</Header>
+                        <Form.Input
+                            placeholder='Write a title'
+                            type="text"
+                            name="title"
+                        />
+                        <Header>Select a 3D Model:</Header>                    
+                        <Select placeholder='Select a 3D Model' options={[{key: "1", value:"nc", text:"nice"}]} />
+                        <Header>Select a thumbnail:</Header>                                                
+                        <UploadButton/>
+                        <Header>Description:</Header>                                    
+                        <textarea rows='5' cols='50' style={{
+                            minHeight: '100px',
+                            maxHeight: '500px',                       
+                            minWidth: '100%', 
+                            maxWidth: '100%' 
+                        }} name="description"
+                        placeholder="Write a description"/>
+                    </Segment>
+                    <Button className="submitButton" type='submit 'color='blue' fluid size='large'>Add post</Button>
+                </Form>
+            </Segment>
+        )
+    }
+}
+
+const UploadButton = () => (
+    <div>
+        <label for="file-upload" class="file-upload">
+            <i className="fa fa-cloud-upload"></i> Upload picture
+            </label>
+        <input id="file-upload" type="file"/>
+    </div>
+)
