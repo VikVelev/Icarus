044a83b1d4c7aa40e71139366b8a48ff49f12ec7
VikVelev <viktorvelev8@gmail.com>
2018-03-27 01:34:47 +0300

    Build 0.17

--- cid-spa/docker_django_deploy.sh
+++ cid-spa/docker_django_deploy.sh
@@ -2,10 +2,22 @@
 # A Script that you should run after 'yarn build' to deploy the SPA to dockerized django
 
 # If you don't have lolcat just remove it. It's just for fun
-echo Deploying... | lolcat -a -d 20
+echo Deploying...
+
+while read version
+do
+    build=$version
+done < "build.ver"
 
 yarn install
 yarn build
 cp -r build/* ./docker_build
 
-echo Deployed | lolcat -a -d 20
+echo "Committing..."
+
+git add . # Adds the built project to the git stash
+build=$((build+1))
+echo $build >> 'build.ver'
+git commit -am "Build 0.$build"
+
+echo "Deployed Build 0.$build"
