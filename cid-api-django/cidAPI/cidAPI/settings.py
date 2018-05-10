35ab669e86769a91b51fd507bda5dc3ba374df65
VikVelev <viktorvelev8@gmail.com>
2018-05-10 12:07:29 +0300

    Added django staticfiledirs

--- cid-api-django/cidAPI/cidAPI/settings.py
+++ cid-api-django/cidAPI/cidAPI/settings.py
@@ -196,7 +196,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/css'),
+    os.path.join(BASE_DIR, 'admin/static/css'),
+    os.path.join(BASE_DIR, 'admin/static/fonts'),
+    os.path.join(BASE_DIR, 'admin/static'),        
 )
 
 STATIC_ROOT = 'static'
